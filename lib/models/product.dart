560370024dec198edb1fd86bd1e730633aa2b6e4
BBarisKilic <bulentbariskilic@gmail.com>
2020-08-29 14:58:14 +0300

    database is ready to go.

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -7,4 +7,21 @@ class Product {
   Product(this.name, this.description, this.unitPrice);
 
   Product.withId(this.id, this.name, this.description, this.unitPrice);
+
+  Map<String, dynamic> toMap() {
+    var dbMap = Map<String, dynamic>();
+    dbMap["name"] = this.name;
+    dbMap["description"] = this.description;
+    dbMap["unitPrice"] = this.unitPrice;
+    if (this.id != null) {
+      dbMap["id"] = this.id;
+    }
+  }
+
+  Product.fromObject(dynamic o) {
+    this.id = int.tryParse(o[id]);
+    this.name = o[name];
+    this.description = o[description];
+    this.unitPrice = double.tryParse(o[unitPrice]);
+  }
 }
